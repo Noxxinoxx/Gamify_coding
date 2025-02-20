adc94fc8c2aba8b47a93b00cc77d5deec0011638
Noa Andersson <noaan869@student.liu.se>
2025-02-21 00:04:36 +0100

    I added the timer class that will handle the time in the game. Next is to add the game loop to the timer class

---
+++ src/timer.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+class Timer:
+
+    def __init__(self, length_of_game, interval):
+        """
+        The init takes a time that is the lenght of the game and a interval that is the interval        between games.
+        """
+        self.length = length_of_game;
+        self.interval = interval;
+        self.current_time = 0;
+
+    def run_game(self):
+         
+        while():
+            print("game is running")
+
+
+

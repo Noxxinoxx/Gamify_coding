54497faac398055d28b912339d284e650dce643a
Noa Andersson <nooaanderssondator@gmail.com>
2025-03-05 01:08:01 +0100

    Added some things to the lp system that needs to be in place before we can add win streak and lose strek.

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
 import os
 
 paths = dict(
-    database_url = os.getcwd() + "../res/Gamify_coding_database.json",  
+    database_url = os.path.join(os.getcwd(), "../res/Gamify_coding_database.json"),  
 )
+game_length = 10;
 check_processes_interval = 10;
 gameMode = "i"
 badProcesses = ["cs2.exe", "LeagueClient.exe"]
-goodProcesses = ["nvim.exe", "terminal.exe"]
+goodProcesses = ["nvim.exe", "terminal.exe"]    
--- src/game.py
+++ src/game.py
@@ -1,16 +1,19 @@
 import config
 import processes
+import tools
 class Game:
     def __init__(self):
         self.game_number = 0;
         self.game_mode = config.gameMode;
-        self.game_running = False; 
+        self.game_running = False;
+        self.user_database = tools.read_from_database();
+        self.user_lp = self.user_database[0]["LP"];
     
-    def update_game_number(self,number):
+    def update_game_number(self):
         """
         updates the game number this is part of the game logic for later.
         """
-        self.game_number = number;
+        self.game_number = self.game_number + 1;
     def game_status(self):
         """
         gets the game status if we have a running game or not.
@@ -39,6 +42,39 @@ class Game:
         returns if the we keep the game running and no bad processes has been started.
         """
         return self.game_check_processes();
+    
+    def win(self):
+        """
+        This function gets called when you win a game.
+        """
+
+        lp_gain = self.user_database[0]["LP_gain"]
+        lp = self.user_lp;
+        new_lp = lp + lp_gain;
+        self.user_lp = new_lp;
+        tools.update_value_in_database("LP", new_lp);
+
+        print(f"Good Job you won the game you got {lp}")
+        
+        
+
+        self.update_game_number();
+        self.change_game_status(False);
+
+
+    def game_lost(self):
+        """
+        This function is called when we lose a game.
+        """    
+        #here we add points that the user lost.
+        print("Game over you lost because you opened a process on the bad list!")
+        self.update_game_number();
+        self.change_game_status(False);    
+
+
+
+
+
 
 
 

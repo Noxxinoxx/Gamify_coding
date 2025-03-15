39f6cf5a7b80ce59cf87c4c005af4379d26b4498
nox <nooaanderssondator@gmail.com>
2025-03-15 14:10:05 +0100

    Began the changes to the linux version of the code. Will add a config varible later named OS.

--- src/processes.py
+++ src/processes.py
@@ -7,19 +7,25 @@ class Processes :
         self.bad_processes = config.badProcesses;
         self.process = wmi.WMI();
 
-    def runCheck(self):
+    def runCheckWindows(self, what_os):
         """
-        Runs the check that looks for bad proceses it counts them and the returns the restult.
+        Runs the check that looks for bad proceses on windows and returns the result.
         """
         bad_processes_running = 0;
         good_processes_running = 0;
         
-        for process in self.process.Win32_Process():
-            for bad_processes in self.bad_processes:
-                if(bad_processes == process.Name):
-                    bad_processes_running +=1
-            for good_processes in self.good_processes:
-                if(good_processes == process.Name):
-                    good_processes_running +=1
+
+        if what_os == "Windows":
+
+            for process in self.process.Win32_Process():
+                for bad_processes in self.bad_processes:
+                    if(bad_processes == process.Name):
+                        bad_processes_running +=1
+                for good_processes in self.good_processes:
+                    if(good_processes == process.Name):
+                        good_processes_running +=1
+
+        elif what_os == "Linux":
+            import
         return dict(bad_processes = bad_processes_running, good_processes = good_processes_running)
 

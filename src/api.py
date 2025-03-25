1534fe233d83af705b4678651685f298069e6233
Noxxinoxx <nooaanderssondator@gmail.com>
2025-03-25 01:13:23 +0100

    Added the router class has not started any impl but some arch has been started

---
+++ src/api.py
@@ -0,0 +1,19 @@
+"""
+This file has some api calls.
+1. game state: /api/game_state
+"""
+
+class Router:
+    """
+    This class is used to route all the api calls from the frontend and the backend.
+    """
+    def __init__(self):
+        """
+        init function for the API class.
+        """
+        
+    def router(self, route):
+        """
+        This function is ment to hadle the api routes.
+        """
+
